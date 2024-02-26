7a9d169f4e608674456da0c1fedd59709071d6ac
user1808 <tomasz.kozak04@gmail.com>
2024-02-26 23:53:57 +0100

    14 - first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,6 @@
-type First<T extends any[]> = any
+type First<T extends unknown[]> = T extends [infer Head, ...args: any[]] ? Head : never;
+
+// T extends [infer Head, ...args: any[]] ? Head : never;
+// T extends [] ? never : T[0];
+// T['length'] extends 0 ? never : T[0];
+// T extends [infer Head, ...infer Tail] ? Head : never;
