105a8c69cc445ec2277bebb44135a9f86a9ee80f
user1808 <tomasz.kozak04@gmail.com>
2024-02-27 15:45:13 +0100

    898 - includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any
+// This Equal comes from type-challenges/utils
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false;
+
+type Includes<T extends readonly any[], U> = T extends [infer Head, ...infer Tail] 
+  ? Equal<Head, U> extends true
+    ? true 
+    : Includes<Tail, U>
+  : false;
