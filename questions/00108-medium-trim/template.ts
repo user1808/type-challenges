3578e640c2641af1f674a4704f1212e657286e8f
user1808 <tomasz.kozak04@gmail.com>
2024-02-28 00:16:37 +0100

    108 - trim

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,17 @@
-type Trim<S extends string> = any
+// My Solution:
+type ReverseString<S extends string> = S extends `${infer Head}${infer Tail}` 
+  ? `${ReverseString<Tail>}${Head}`
+  : '';
+
+type Trim<S extends string, Reversed extends boolean = false> = S extends `${infer Head}${infer Tail}`
+  ? Head extends ' ' | '\t' | '\n' 
+    ? Trim<Tail, Reversed>
+    : Reversed extends true 
+      ? ReverseString<`${Head}${Tail}`>
+      : Trim<ReverseString<`${Head}${Tail}`>, true>
+  : '';
+
+// Much simpler solution, but unfortunately not mine
+// type Space = ' ' | '\t' | '\n';
+
+// type Trim<S extends string> = S extends `${Space}${infer T}` | `${infer T}${Space}` ? Trim<T> : S;
