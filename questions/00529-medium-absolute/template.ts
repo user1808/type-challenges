6b66c96536486ac5a023e0db18cbb48a362871ef
user1808 <tomasz.kozak04@gmail.com>
2024-02-29 00:20:53 +0100

    529 - absolute

--- questions/00529-medium-absolute/template.ts
+++ questions/00529-medium-absolute/template.ts
@@ -1,1 +1,13 @@
-type Absolute<T extends number | string | bigint> = any
+// My Solution
+// type Absolute<T extends number | string | bigint> = keyof {
+//   [K in T as `${K}`]: unknown;
+// } extends `${infer Head}${infer Tail}` 
+//   ? Head extends '-'
+//     ? Tail
+//     : `${Head}${Tail}`
+//   : never;
+
+// Much simpler solution
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer R}` 
+  ? R 
+  : `${T}`;
