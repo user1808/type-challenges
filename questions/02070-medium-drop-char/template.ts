ff0e56950ab127c9a09dc5a3f9bf9b69233adfeb
user1808 <tomasz.kozak04@gmail.com>
2024-02-29 19:38:36 +0100

    2070 - drop char

--- questions/02070-medium-drop-char/template.ts
+++ questions/02070-medium-drop-char/template.ts
@@ -1,1 +1,8 @@
-type DropChar<S, C> = any
+type DropChar<S extends string, C extends string, Acc extends string = ''> = S extends `${infer Head}${infer Tail}`
+  ? Head extends C
+    ? DropChar<Tail, C, Acc>
+    : DropChar<Tail, C, `${Acc}${Head}`>
+  : Acc;
+
+// Other Solution
+// type DropChar<S, C extends string> = S extends `${infer L}${C}${infer R}` ? DropChar<`${L}${R}`, C> : S;
