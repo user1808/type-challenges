946f1884b0d99632518f304fdf189c79293f4ca3
user1808 <tomasz.kozak04@gmail.com>
2024-02-29 14:33:39 +0100

    645 - diff

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,10 @@
-type Diff<O, O1> = any
+type Diff<O extends object, O1 extends object> = Omit<O & O1, keyof (O | O1)>;
+// My Solution (much more complicated)
+// {
+//   [K in Exclude<keyof O, keyof O1> | Exclude<keyof O1, keyof O>]: 
+//     K extends keyof O 
+//       ? O[K] 
+//       : K extends keyof O1 
+//         ? O1[K] 
+//         : never;
+// }
