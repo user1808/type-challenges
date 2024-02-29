1a2d9b56718fc2653c8c8e448fdfb1e10b0963f4
user1808 <tomasz.kozak04@gmail.com>
2024-02-29 19:28:38 +0100

    1978 - percentage parser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,9 @@
-type PercentageParser<A extends string> = any
+type PercentageParser<A extends string, Acc extends string[] = ['', '', '']> = A extends `${infer Head}${infer Tail}`
+  ? Head extends '+' | '-'
+    ? PercentageParser<Tail, [Head, Acc[1], Acc[2]]>
+    : Head extends '%'
+      ? PercentageParser<Tail, [Acc[0], Acc[1], Head]>
+      : Head extends `${number}`
+        ? PercentageParser<Tail, [Acc[0], `${Acc[1]}${Head}`, Acc[2]]>
+        : PercentageParser<Tail, Acc>
+  : Acc;
