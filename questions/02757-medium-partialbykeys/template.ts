2f3ae7ac3e8854bd2bf961a46ae9631f47e68006
user1808 <tomasz.kozak04@gmail.com>
2024-02-29 21:48:20 +0100

    2757 - partial by keys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,7 @@
-type PartialByKeys<T, K> = any
+type Flat<T extends object> = {
+  [K in keyof T]: T[K]
+}
+
+type PartialByKeys<T, K extends keyof T | never = never> = [K] extends [never] 
+  ? { [U in keyof T]?: T[U] }
+  : Flat<Omit<T, K> & { [U in keyof T as U extends K ? U : never]?: T[U] }>
