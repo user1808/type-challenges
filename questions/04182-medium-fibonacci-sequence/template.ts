32459fba3f4df1c384cecc3e3b1e56a7ac816586
user1808 <tomasz.kozak04@gmail.com>
2024-03-07 23:51:21 +0100

    4182 - fibonacci

--- questions/04182-medium-fibonacci-sequence/template.ts
+++ questions/04182-medium-fibonacci-sequence/template.ts
@@ -1,1 +1,9 @@
-type Fibonacci<T extends number> = any
+type Fibonacci<T extends number, Acc extends unknown[] = [], T0 extends unknown[] = [], T1 extends unknown[] = []> = 
+  Acc['length'] extends T
+    ? [...T0, ...T1]['length']
+    : Fibonacci<
+        T, 
+        [unknown, ...Acc], 
+        Acc['length'] extends 0 | 1 ? T0 : T1, 
+        Acc['length'] extends 0 | 1 ? [unknown] : [...T0, ...T1]
+      >;
