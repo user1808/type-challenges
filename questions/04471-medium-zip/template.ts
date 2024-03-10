ced988fbe4b7226273b631a679e22cd6e3f37c1f
user1808 <tomasz.kozak04@gmail.com>
2024-03-10 22:38:42 +0100

    4471 - zip

--- questions/04471-medium-zip/template.ts
+++ questions/04471-medium-zip/template.ts
@@ -1,1 +1,6 @@
-type Zip<T, U> = any
+// Yea, i know that T and U are suppose to be Tuples but whatever, I leave it as it is
+type Zip<T extends unknown[], U extends unknown[], Acc extends unknown[] = []> = Acc['length'] extends 2
+  ? Acc
+  : Acc['length'] extends T['length'] | U['length']
+    ? Acc
+    : Zip<T, U, [...Acc, [T[Acc['length']], U[Acc['length']]]]>
