045d09e666f7f4218f78c0fc382383e4f7f01e62
user1808 <tomasz.kozak04@gmail.com>
2024-03-12 21:14:16 +0100

    4499 - chunk

--- questions/04499-medium-chunk/template.ts
+++ questions/04499-medium-chunk/template.ts
@@ -1,1 +1,8 @@
-type Chunk = any
+type Chunk<T extends unknown[], U extends number, Acc extends unknown[] = [], AccPart extends unknown[] = []> = 
+  T extends [infer Head, ...infer Tail]
+    ? AccPart['length'] extends U
+      ? Chunk<Tail, U, [...Acc, AccPart], [Head]>
+      : Chunk<Tail, U, Acc, [...AccPart, Head]>
+    : AccPart['length'] extends 0 
+      ? Acc 
+      : [...Acc, AccPart];
