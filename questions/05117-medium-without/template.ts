1519f1b8a4c9449f0198de0d05b73315b5fd5d25
user1808 <tomasz.kozak04@gmail.com>
2024-03-12 21:51:18 +0100

    5117 - without

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,6 @@
-type Without<T, U> = any
+type Without<T extends unknown[], U extends unknown[] | unknown, Acc extends unknown[] = []> = 
+  T extends [infer Head, ...infer Tail]
+    ? Head extends (U extends unknown[] ? U[number] : U)
+      ? Without<Tail, U, Acc>
+      : Without<Tail, U, [...Acc, Head]>
+    : Acc
