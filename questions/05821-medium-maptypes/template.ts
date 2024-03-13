83739d95443ab6d4ff0574c34c3eb868e876d902
user1808 <tomasz.kozak04@gmail.com>
2024-03-13 23:55:41 +0100

    5821 - map types

--- questions/05821-medium-maptypes/template.ts
+++ questions/05821-medium-maptypes/template.ts
@@ -1,1 +1,11 @@
-type MapTypes<T, R> = any
+type Helper<T extends {}, U extends {}> = {
+  [K in keyof T]: K extends keyof U ? U[K] : T[K];
+};
+
+type MapTypes<T extends {}, R extends { mapFrom: unknown; mapTo: unknown }> = Helper<T, {
+  [K in keyof T as R extends any ? T[K] extends R['mapFrom'] ? K : never : never]: R extends any
+    ? T[K] extends R['mapFrom']
+      ? R['mapTo']
+      : never
+    : never;
+}>;
