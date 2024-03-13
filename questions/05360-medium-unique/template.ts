98c7bfa61ec8a7891cdaa5d254ee4a62c2e5eb76
user1808 <tomasz.kozak04@gmail.com>
2024-03-13 22:56:06 +0100

    5360 - unique

--- questions/05360-medium-unique/template.ts
+++ questions/05360-medium-unique/template.ts
@@ -1,1 +1,5 @@
-type Unique<T> = any
+type Unique<T extends unknown[], Acc extends unknown[] = []> = T extends [infer Head, ...infer Tail]
+  ? IndexOf<Acc, Head> extends -1
+    ? Unique<Tail, [...Acc, Head]>
+    : Unique<Tail, Acc>
+  : Acc
