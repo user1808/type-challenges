04f36d72c9629db20e75004c112d19081aee6fdb
user1808 <tomasz.kozak04@gmail.com>
2024-03-13 21:10:02 +0100

    5153 - indexof

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,5 @@
-type IndexOf<T, U> = any
+type IndexOf<T extends unknown[], U, Acc extends unknown[] = []> = T extends [infer Head, ...infer Tail]
+  ? Equal<U, Head> extends true
+    ? Acc['length']
+    : IndexOf<Tail, U, [unknown, ...Acc]>
+  : -1
