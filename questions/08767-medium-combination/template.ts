574eb84a97f10c3c61fd89994c50a33b260a53a0
user1808 <tomasz.kozak04@gmail.com>
2024-03-14 21:51:49 +0100

    8767 - combination

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -1,1 +1,6 @@
-type Combination<T extends string[]> = any
+// Slightly modified exercise 4260
+type Combination<T extends string[], Union extends string = '', UnionCopy extends string = Union> =  T extends [infer Head extends string, ...infer Tail extends string[]]
+  ? Combination<Tail, Head | Union>
+  : Union extends infer UnionElement 
+    ? UnionElement extends '' ? never : UnionElement | (UnionElement extends string ? `${UnionElement} ${Combination<T, Exclude<UnionCopy, UnionElement>>}` : never)
+    : never;
