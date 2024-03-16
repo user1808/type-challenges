3a9010b5f6be22fe2058cd05c6f6a38fd44dc8e7
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 20:37:56 +0100

    21106 - combs

--- questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
+++ questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/template.ts
@@ -1,2 +1,6 @@
 // 实现 Combs
-type Combs<T extends any[]> = any
+type Combs<T extends string[]> = T extends [infer Head extends string, ...infer Tail extends string[]]
+  ? Combs<Tail> | (Tail[number] extends any ? 
+      `${Head} ${Tail[number]}` 
+      : never)
+  : never
--- questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/test-cases.ts
+++ questions/21106-medium-zu-he-jian-lei-xing-combination-key-type/test-cases.ts
@@ -3,6 +3,8 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type ModifierKeys = ['cmd', 'ctrl', 'opt', 'fn']
 type CaseTypeOne = 'cmd ctrl' | 'cmd opt' | 'cmd fn' | 'ctrl opt' | 'ctrl fn' | 'opt fn'
 
+type A = Combs<ModifierKeys>;
+
 type cases = [
   Expect<Equal<Combs<ModifierKeys>, CaseTypeOne>>,
 ]
