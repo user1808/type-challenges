7cc564325fff90c890430b5fcff514a1047f0d66
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 18:34:40 +0100

    9896 - get middle element

--- questions/09896-medium-get-middle-element/template.ts
+++ questions/09896-medium-get-middle-element/template.ts
@@ -1,1 +1,5 @@
-type GetMiddleElement<T> = any
+type AAA = GetMiddleElement<[1, 2]>;
+
+type GetMiddleElement<T extends unknown[]> = T extends [infer _, ...infer B, infer _]
+  ? T['length'] extends 2 ? T : GetMiddleElement<B>
+  : T;
