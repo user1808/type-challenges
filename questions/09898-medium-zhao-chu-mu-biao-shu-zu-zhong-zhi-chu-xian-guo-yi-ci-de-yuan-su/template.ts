f3033c30a94d2231c1c453e8cf2aed9b7395f734
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 19:05:55 +0100

    9989 - Count Element Number To Object

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,5 +1,3 @@
-type A = FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 1]>;
-
 type FindEles<T extends unknown[], VisitedElements extends unknown[] = [], Acc extends unknown[] = []> = 
   T extends [infer Head, ...infer Tail]
     ? FindEles<
