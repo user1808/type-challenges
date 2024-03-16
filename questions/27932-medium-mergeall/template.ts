931937109c01f15e3e7b32aa5eab6da6f89b42af
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 23:36:53 +0100

    27932 - merge all

--- questions/27932-medium-mergeall/template.ts
+++ questions/27932-medium-mergeall/template.ts
@@ -1,1 +1,10 @@
-type MergeAll<XS> = any
+type MergeAll<XS extends object[], Acc extends object = {}> = 
+  XS extends [infer Head extends object, ...infer Tail extends object[]]
+    ? MergeAll<Tail, {
+      [K in keyof Acc | keyof Head]: K extends keyof Head
+        ? K extends keyof Acc
+          ? Acc[K] | Head[K]
+          : Head[K]
+        : K extends keyof Acc ? Acc[K] : never
+    }>
+    : Acc
