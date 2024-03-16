3975c2655b98f37ad1eee6665e8b8cdbb003d455
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 19:37:55 +0100

    17973 - deep mutable

--- questions/17973-medium-deepmutable/template.ts
+++ questions/17973-medium-deepmutable/template.ts
@@ -1,1 +1,6 @@
-type DeepMutable = any
+type DeepMutable<T extends object | readonly unknown[]> = {
+  -readonly [K in keyof T]: 
+    T[K] extends Record<keyof any, unknown> | readonly unknown[]
+      ? DeepMutable<T[K]> 
+      : T[K] 
+};
