75e15f7b108c057bc26449f03f1c58a2405ae6ee
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 00:10:56 +0100

    30301 - is odd

--- questions/30301-medium-isodd/template.ts
+++ questions/30301-medium-isodd/template.ts
@@ -1,1 +1,9 @@
-type IsOdd<T extends number> = any
+type IsOddHelper<S extends string> = S extends `${infer Head}${infer Tail}`
+  ? [...IsOddHelper<Tail>, Head]
+  : []
+
+type IsOdd<T extends number> = number extends T
+  ? false
+  : IsOddHelper<`${T}`>[0] extends '0' | '2' | '4' | '6' | '8'
+    ? false
+    : true;
