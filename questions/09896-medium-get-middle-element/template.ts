5965fa4ed89d7e28086d4581510e276ff93daaa1
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 22:16:52 +0100

    25270 - transpose

--- questions/09896-medium-get-middle-element/template.ts
+++ questions/09896-medium-get-middle-element/template.ts
@@ -1,5 +1,3 @@
-type AAA = GetMiddleElement<[1, 2]>;
-
 type GetMiddleElement<T extends unknown[]> = T extends [infer _, ...infer B, infer _]
   ? T['length'] extends 2 ? T : GetMiddleElement<B>
   : T;
--- questions/10969-medium-integer/template.ts
+++ questions/10969-medium-integer/template.ts
@@ -1,5 +1,3 @@
-type A = Integer<1.1>;
-
 type Integer<T extends number> = `${T}` extends `${any}.${any}`
   ? never
   : number extends T ? never : T;
