0439a75d9bb9b83a37add85edfed687c4bb5f79a
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 18:47:53 +0100

    9898 - appear only once

--- questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
+++ questions/09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/template.ts
@@ -1,1 +1,10 @@
-type FindEles<T extends any[]> = any
+type A = FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 1]>;
+
+type FindEles<T extends unknown[], VisitedElements extends unknown[] = [], Acc extends unknown[] = []> = 
+  T extends [infer Head, ...infer Tail]
+    ? FindEles<
+      Tail,
+      [...VisitedElements, Head], 
+      Head extends Tail[number] | VisitedElements[number] ? Acc : [...Acc, Head]
+    >
+    : Acc
