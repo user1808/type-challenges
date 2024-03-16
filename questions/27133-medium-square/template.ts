da144269d77f9c96580005431a0317e4459524ac
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 22:42:33 +0100

    27133 - square

--- questions/27133-medium-square/template.ts
+++ questions/27133-medium-square/template.ts
@@ -1,1 +1,18 @@
-type Square<N extends number> = number
+type Square<
+  N extends number,
+  StepAcc extends unknown[] = [], 
+  StepsCount extends unknown[] = [], 
+  Acc extends unknown[] = [],
+  AddZeros extends string = '',
+> = 
+  `${N}` extends `-${infer R1 extends number}` 
+    ? Square<R1, [], [], [], AddZeros>
+    : `${N}` extends `${infer R2 extends number}0`
+      ? Square<R2, [], [], [], `${AddZeros}00`>
+      : StepsCount['length'] extends N
+        ? `${Acc['length']}${AddZeros}` extends `${infer R3 extends number}` 
+          ? R3 // <--- result
+          : never 
+        : StepAcc['length'] extends N
+          ? Square<N, [], [unknown, ...StepsCount], [...Acc, ...StepAcc], AddZeros>
+          : Square<N, [unknown, ...StepAcc], StepsCount, Acc, AddZeros>
