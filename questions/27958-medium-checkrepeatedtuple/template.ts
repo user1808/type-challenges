3e7b2eead1e56876cc3703a281cc4d5e70e2e1d9
user1808 <tomasz.kozak04@gmail.com>
2024-03-16 23:42:47 +0100

    27958 - check repeated tuple

--- questions/27958-medium-checkrepeatedtuple/template.ts
+++ questions/27958-medium-checkrepeatedtuple/template.ts
@@ -1,1 +1,6 @@
-type CheckRepeatedTuple<T extends unknown[]> = any
+type CheckRepeatedTuple<T extends unknown[], Pre extends unknown[] = []> = 
+  T extends [infer Head, ...infer Tail]
+    ? Head extends Tail[number]
+      ? true
+      : CheckRepeatedTuple<Tail, [...Pre, Head]>
+    : false
