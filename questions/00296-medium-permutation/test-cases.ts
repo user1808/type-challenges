39636d353942800f4fa021a97cc32cbd5a3c62b2
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 19:38:11 +0100

    21220 - permutation of tuple

--- questions/00296-medium-permutation/test-cases.ts
+++ questions/00296-medium-permutation/test-cases.ts
@@ -1,7 +1,5 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type Test = Permutation<'A' | 'B' | 'C'>;
-
 type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
   Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
--- questions/08987-medium-subsequence/template.ts
+++ questions/08987-medium-subsequence/template.ts
@@ -1,5 +1,3 @@
-type Test = Subsequence<[1, 2]>;
-
 type Subsequence<T extends any[]> = 
   T extends [infer Head, ...infer Tail]
     ? Subsequence<Tail> | [Head, ...Subsequence<Tail>]
