cd2b6f3473995e61b3a5b7964e9f30c707fefd1d
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 20:54:23 +0100

    17 - currying 1

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,11 @@
-declare function Currying(fn: any): any
+type CurryingHelper<T extends unknown[], Result> = 
+  T extends [any, ...infer Tail]
+    ? T extends [...infer Head, ...Tail]
+      ? (...args: Head) => CurryingHelper<Tail, Result>
+      : never
+    : Result;
+
+declare function Currying<T>(fn: T): 
+  T extends (...args: infer R) => infer S
+  ? R extends [] ? () => S : CurryingHelper<R, S>
+  : never;
