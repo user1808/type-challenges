b360390fe0e865b70d4afe72c14896ed73f18cec
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 15:17:52 +0100

    20430 - tower of hanoi

--- questions/30430-medium-tower-of-hanoi/template.ts
+++ questions/30430-medium-tower-of-hanoi/template.ts
@@ -1,1 +1,13 @@
-type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = any
+type HanoiHelper<N extends number, Acc extends unknown[] = []> = 
+  Acc['length'] extends N
+    ? Acc extends [infer _, ...infer Tail] ? Tail['length'] : 0
+    : HanoiHelper<N, [N, ...Acc]>;
+
+type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = 
+  N extends 0
+    ? []
+    : [
+        ...Hanoi<HanoiHelper<N>, From, Intermediate, To>,
+        [From, To],
+        ...Hanoi<HanoiHelper<N>, Intermediate, To, From>,
+      ];
