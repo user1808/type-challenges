33cebe6a6c8a8bf862dbd08bdb0a486907980acd
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 19:12:01 +0100

    8987 - subsequence

--- questions/08987-medium-subsequence/template.ts
+++ questions/08987-medium-subsequence/template.ts
@@ -1,1 +1,6 @@
-type Subsequence<T extends any[]> = any
+type Test = Subsequence<[1, 2]>;
+
+type Subsequence<T extends any[]> = 
+  T extends [infer Head, ...infer Tail]
+    ? Subsequence<Tail> | [Head, ...Subsequence<Tail>]
+    : []
