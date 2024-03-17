fc288624ed095ad49208cbffbb86435318675410
user1808 <tomasz.kozak04@gmail.com>
2024-03-17 18:08:19 +0100

    remove test type

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,6 +1,3 @@
-const testTuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
-type Test = typeof testTuple[number];
-
 type TupleToObject<T extends readonly (keyof any)[]> = {
   [P in T[number]]: P
 }
