a0391c0d09220d50e49265647b5e1d5c85bf0105
user1808 <tomasz.kozak04@gmail.com>
2024-03-18 21:41:09 +0100

    112 - capitalize words

--- questions/00112-hard-capitalizewords/template.ts
+++ questions/00112-hard-capitalizewords/template.ts
@@ -1,1 +1,8 @@
-type CapitalizeWords<S extends string> = any
+type CapitalizeWords<
+  S extends string,
+  Word extends string = ''
+> = S extends `${infer Head}${infer Tail}`
+  ? Uppercase<Head> extends Lowercase<Head>
+    ? `${Capitalize<`${Word}${Head}`>}${CapitalizeWords<Tail>}`
+    : CapitalizeWords<Tail, `${Word}${Head}`>
+  : Capitalize<Word>
