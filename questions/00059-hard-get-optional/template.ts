e1c26375ecdb43ecd95426bbae064898e0781287
user1808 <tomasz.kozak04@gmail.com>
2024-03-18 20:45:29 +0100

    89 - required keys

--- questions/00059-hard-get-optional/template.ts
+++ questions/00059-hard-get-optional/template.ts
@@ -1,5 +1,3 @@
-type Test = GetOptional<{ foo: number, bar?: string }>;
-
 type GetOptional<T extends object> = {
   [Key in keyof T as T[Key] extends Required<T>[Key] ? never : Key]: T[Key]
 }
--- questions/00089-hard-required-keys/template.ts
+++ questions/00089-hard-required-keys/template.ts
@@ -1,1 +1,3 @@
-type RequiredKeys<T> = any
+type RequiredKeys<T extends object> = keyof {
+  [Key in keyof T as T[Key] extends Required<T>[Key] ? Key : never]: T[Key]
+}
