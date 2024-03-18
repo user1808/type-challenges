510feeb6cb07178c75774067b10fb5470918b96e
user1808 <tomasz.kozak04@gmail.com>
2024-03-18 22:32:53 +0100

    147 - c printf parser

--- questions/00147-hard-c-printf-parser/template.ts
+++ questions/00147-hard-c-printf-parser/template.ts
@@ -8,4 +8,12 @@ type ControlsMap = {
   p: 'pointer'
 }
 
-type ParsePrintFormat = any
+type ParsePrintFormat<S extends string> = S extends `${infer Head}${infer Tail}`
+  ? Head extends '%'
+    ? Tail extends `${infer TailHead}${infer TailTail}`
+      ? TailHead extends keyof ControlsMap
+        ? [ControlsMap[TailHead], ...ParsePrintFormat<TailTail>]
+        : ParsePrintFormat<TailTail>
+      : []
+    : ParsePrintFormat<Tail>
+  : []
