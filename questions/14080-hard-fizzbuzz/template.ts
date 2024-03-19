10ec79f62947e1eb02d7d6d043fac12173a96d1f
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 00:18:37 +0100

    14080 - fizzbuzz

--- questions/14080-hard-fizzbuzz/template.ts
+++ questions/14080-hard-fizzbuzz/template.ts
@@ -1,1 +1,23 @@
-type FizzBuzz<N extends number> = any
+type IsDivisibleBy5<N extends number> = `${N}` extends `${string}${'0' | '5'}`
+  ? true
+  : false;
+
+type IsDivisibleBy3<N extends number, Acc extends unknown[] = []> = 
+  `${N}` extends `${infer Head extends number}${infer Tail extends number}`
+    ? IsDivisibleBy3<Tail, [...CreateArrayOfLength<Head>, ...Acc]> 
+    : [...CreateArrayOfLength<N>, ...Acc]['length'] extends 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+      ? [...CreateArrayOfLength<N>, ...Acc]['length'] extends 3 | 6 | 9 ? true : false
+      : [...CreateArrayOfLength<N>, ...Acc]['length'] extends number 
+        ? IsDivisibleBy3<[...CreateArrayOfLength<N>, ...Acc]['length']>
+        : never;
+
+type FizzBuzz<N extends number, Acc extends string[] = []> = 
+  Acc['length'] extends N
+    ? Acc
+    : IsDivisibleBy5<['', ...Acc]['length']> extends true
+      ? IsDivisibleBy3<['', ...Acc]['length']> extends true
+        ? FizzBuzz<N, [...Acc, 'FizzBuzz']>
+        : FizzBuzz<N, [...Acc, 'Buzz']>
+      : IsDivisibleBy3<['', ...Acc]['length']> extends true
+        ? FizzBuzz<N, [...Acc, 'Fizz']>
+        : FizzBuzz<N, [...Acc, `${['', ...Acc]['length']}`]>
