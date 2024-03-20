22a8b09aa2cbbd1b7d187b5eb1995244783beddf
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 23:24:41 +0100

    14188 - run length encoding (decode)

--- questions/14188-hard-run-length-encoding/template.ts
+++ questions/14188-hard-run-length-encoding/template.ts
@@ -10,5 +10,15 @@ namespace RLE {
       : `${SameCharCountLength extends 1 | 0 ? '' : SameCharCountLength}${LastChar}${Encode<Tail, Head, [unknown]>}`
     : `${SameCharCountLength extends 1 | 0 ? '' : SameCharCountLength}${string extends LastChar ? '' : LastChar}`;
 
-  export type Decode<S extends string> = any;
+  export type Decode<
+    S extends string,
+    HowMany extends number = 1,
+    Count extends unknown[] = []
+  > = S extends `${infer Count extends number}${infer Letter}${infer Tail}`
+    ? Decode<`${Letter}${Tail}`, Count>
+    : S extends `${infer Letter}${infer Tail}`
+      ? Count['length'] extends HowMany
+        ? Decode<Tail>
+        : `${Letter}${Decode<`${Letter}${Tail}`, HowMany, [unknown, ...Count]>}`
+      : '';
 }
