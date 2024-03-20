83e3d8459752c40b05290be7dc1bc4cacf48ed5e
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 16:17:56 +0100

    6141 - binary to decimal

--- questions/05181-hard-mutable-keys/template.ts
+++ questions/05181-hard-mutable-keys/template.ts
@@ -1,5 +1,3 @@
-type Test = MutableKeys<{ a: number, readonly b: string }> ;
-
 type MutableKeys<T extends object> = keyof {
   [K in keyof T as Equal<Readonly<Pick<T, K>>, Pick<T, K>> extends true ? never : K]: ''
 }
--- questions/06141-hard-binary-to-decimal/template.ts
+++ questions/06141-hard-binary-to-decimal/template.ts
@@ -1,1 +1,21 @@
-type BinaryToDecimal<S extends string> = any
+type PowerOfTwo<
+  N extends number, 
+  NCount extends unknown[] = [], 
+  Acc extends unknown[] = [unknown]
+> = NCount['length'] extends N
+  ? Acc
+  : PowerOfTwo<N, [unknown, ...NCount], [...Acc, ...Acc]>;
+
+type BinaryToDecimalHelper<
+  S extends string, 
+  Count extends unknown[] = [], 
+  Acc extends unknown[] = []
+> = S extends `${infer Head extends '0' | '1'}${infer Tail}`
+  ? BinaryToDecimalHelper<
+    Tail, 
+    [unknown, ...Count], 
+    [...Acc, ...(Head extends '0' ? [] : PowerOfTwo<Count['length']>)]
+  >
+  : Acc['length'];
+
+type BinaryToDecimal<S extends string> = BinaryToDecimalHelper<ReverseString<S>>;
