2429017a070fd22f10bf0157faa7e559dc8115f5
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 01:12:15 +0100

    5181 - mutable keys

--- questions/05181-hard-mutable-keys/template.ts
+++ questions/05181-hard-mutable-keys/template.ts
@@ -1,1 +1,5 @@
-type MutableKeys<T> = any
+type Test = MutableKeys<{ a: number, readonly b: string }> ;
+
+type MutableKeys<T extends object> = keyof {
+  [K in keyof T as Equal<Readonly<Pick<T, K>>, Pick<T, K>> extends true ? never : K]: ''
+}
