8571ac533df134e4685095a1b73003983902001d
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 16:56:27 +0100

    28143 - optional undefined

--- questions/28143-hard-optionalundefined/template.ts
+++ questions/28143-hard-optionalundefined/template.ts
@@ -1,1 +1,11 @@
-type OptionalUndefined<T, Props> = any
+type Test = OptionalUndefined<{ value: string | undefined, desc: string }, 'value'>;
+
+type OptionalUndefinedMerge<
+  Original extends object, 
+  WithOptional extends object, 
+  Together = WithOptional & Omit<Original, keyof WithOptional>
+> = { [K in keyof Together] : Together[K] };
+
+type OptionalUndefined<T extends object, Props extends string = string> = OptionalUndefinedMerge<T, {
+  [K in keyof T as K extends Props ? undefined extends T[K] ? K : never : never]?: T[K];
+}>;
