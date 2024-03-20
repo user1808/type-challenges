a49f4e5c3bccaceb045c7423f34a7cbe0800507a
user1808 <tomasz.kozak04@gmail.com>
2024-03-20 15:54:43 +0100

    9160 - assign

--- questions/09160-hard-assign/template.ts
+++ questions/09160-hard-assign/template.ts
@@ -1,1 +1,7 @@
-type Assign<T extends Record<string, unknown>, U> = any
+type Assign<T extends Record<string, unknown>, U extends unknown[]> =
+  U extends [infer Head extends Record<string, unknown>, ...infer Tail extends Record<string, unknown>[]]
+    ? Assign<
+      { [K in keyof T | keyof Head]: K extends keyof Head ? Head[K] : K extends keyof T ? T[K] : never },
+      Tail
+    >
+    : T
