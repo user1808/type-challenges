cf163d85b1ffd3f35f15a6ea845dc4d81be20fc8
user1808 <tomasz.kozak04@gmail.com>
2024-03-21 18:00:49 +0100

    9775 - capitalize nest object keys

--- questions/09775-hard-capitalize-nest-object-keys/template.ts
+++ questions/09775-hard-capitalize-nest-object-keys/template.ts
@@ -1,1 +1,18 @@
-type CapitalizeNestObjectKeys<T> = any
+type CapitalizeNestArrayKeys<T extends unknown[]> = T extends [infer Head, ...infer Tail]
+  ? [
+      Head extends unknown[] 
+        ? CapitalizeNestArrayKeys<Head> 
+        : Head extends object 
+          ? CapitalizeNestObjectKeys<Head> 
+          : Head, ...CapitalizeNestArrayKeys<Tail>
+    ]
+  : []
+
+type CapitalizeNestObjectKeys<T extends object> = {
+  [K in keyof T as K extends string ? Capitalize<K> : never]: 
+    T[K] extends unknown[] 
+      ? CapitalizeNestArrayKeys<T[K]>
+      : T[K] extends object
+        ? CapitalizeNestObjectKeys<T[K]>
+        : T[K];
+}
