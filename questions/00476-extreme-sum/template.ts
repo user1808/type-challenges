c13f6e6612d3979667123f3246df729a725b6bc6
user1808 <tomasz.kozak04@gmail.com>
2024-03-21 22:11:45 +0100

    476 - sum

--- questions/00476-extreme-sum/template.ts
+++ questions/00476-extreme-sum/template.ts
@@ -1,1 +1,21 @@
-type Sum<A extends string | number | bigint, B extends string | number | bigint> = string
+type SumDigits<
+  A1 extends number, 
+  A2 extends number,
+  Acc extends unknown[][] = [[], []]
+> = Acc[0]['length'] extends A1
+  ? Acc[1]['length'] extends A2
+    ? [...Acc[0], ...Acc[1]]['length']
+    : SumDigits<A1, A2, [Acc[0], [unknown, ...Acc[1]]]>
+  : SumDigits<A1, A2, [[unknown, ...Acc[0]], Acc[1]]>;
+
+type SumHelper<A extends string, B extends string, Rest extends number = 0> = 
+  A extends `${infer HeadA extends number}${infer TailA}`
+    ? B extends `${infer HeadB extends number}${infer TailB}`
+      ? `${SumDigits<SumDigits<HeadA, HeadB>, Rest>}` extends `${infer SumRest extends number}${infer Result extends number}`
+        ? `${Result}${SumHelper<TailA, TailB, SumRest>}`
+        : `${SumDigits<SumDigits<HeadA, HeadB>, Rest>}${SumHelper<TailA, TailB, 0>}`
+      : Rest extends 0 ? A : SumHelper<A, '0', Rest>
+    : Rest extends 0 ? B : SumHelper<B, '0', Rest>;
+
+type Sum<A extends string | number | bigint, B extends string | number | bigint> = 
+  ReverseString<SumHelper<ReverseString<`${A}`>, ReverseString<`${B}`>>>;
