cf180d8087f02951e6214a5f76a80dd850942571
user1808 <tomasz.kozak04@gmail.com>
2024-03-21 13:13:25 +0100

    1383 - camelize

--- questions/01383-hard-camelize/template.ts
+++ questions/01383-hard-camelize/template.ts
@@ -1,1 +1,19 @@
-type Camelize<T> = any
+type CamelizeArray<T extends unknown[]> = T extends [infer Head, ...infer Tail]
+  ? [
+      Head extends unknown[] 
+        ? CamelizeArray<Head>
+        : Head extends object
+          ? Camelize<Head>
+          : Head,
+      ...CamelizeArray<Tail>,
+    ]
+  : []
+
+type Camelize<T extends object> = {
+  [K in keyof T as CamelCase<K & string>]: 
+    T[K] extends unknown[] 
+      ? CamelizeArray<T[K]> 
+      : T[K] extends object
+        ? Camelize<T[K]>
+        : T[K];
+}
