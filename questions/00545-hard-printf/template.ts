5c2b109d0dc709d2f884eb7f4ac0189e7a6dcf8d
user1808 <tomasz.kozak04@gmail.com>
2024-03-21 11:58:00 +0100

    545 - printf

--- questions/00545-hard-printf/template.ts
+++ questions/00545-hard-printf/template.ts
@@ -1,1 +1,10 @@
-type Format<T extends string> = any
+type FormatDict = {
+  's': string;
+  'd': number;
+}
+
+type Format<T extends string> = T extends `${string}%${infer B}${infer Rest}`
+  ? B extends keyof FormatDict
+    ? (arg: FormatDict[B]) => Format<Rest>
+    : Format<Rest>
+  : string;
