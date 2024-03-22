2d4ea95264029f6bac07a43b726b7e3979cb1822
user1808 <tomasz.kozak04@gmail.com>
2024-03-22 18:45:37 +0100

    734 - inclusive range

--- questions/00734-extreme-inclusive-range/template.ts
+++ questions/00734-extreme-inclusive-range/template.ts
@@ -1,1 +1,8 @@
-type InclusiveRange<Lower extends number, Higher extends number> = any
+type InclusiveRange<Lower extends number, Higher extends number, Acc extends number[] = [Lower]> = 
+  GreaterThan<Lower, Higher> extends true 
+    ? []
+    : Acc[MinusOne<Acc['length']>] extends Higher
+      ? Acc
+      : Sum<Acc[MinusOne<Acc['length']>], 1> extends `${infer R extends number}`
+        ? InclusiveRange<Lower, Higher, [...Acc, R]>
+        : never;
