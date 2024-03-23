b47acb321d0f6081b3c551ac6e63fc79793f5d99
user1808 <tomasz.kozak04@gmail.com>
2024-03-23 17:48:13 +0100

    31447 - count reverse pairs

--- questions/31447-extreme-countreversepairs/template.ts
+++ questions/31447-extreme-countreversepairs/template.ts
@@ -1,1 +1,14 @@
-type CountReversePairs<T extends number[]> = any
+type PlusOne<T extends unknown[]> = [unknown, ...T]['length'];
+
+type CountReversePairsComparator<A extends number, B extends number> = 
+  Comparator<A, B> extends Comparison.Greater ? [unknown] : [];
+
+type CountReversePairs<
+  T extends number[], 
+  Acc extends unknown[] = [], 
+  I extends unknown[] = [], J extends unknown[] = [unknown, ...I]
+> = PlusOne<I> extends T['length']
+  ? Acc['length']
+  : PlusOne<J> extends T['length']
+    ? CountReversePairs<T, [...Acc, ...CountReversePairsComparator<T[I['length']], T[J['length']]>], [unknown, ...I]>
+    : CountReversePairs<T, [...Acc, ...CountReversePairsComparator<T[I['length']], T[J['length']]>], I, [unknown, ...J]>;
