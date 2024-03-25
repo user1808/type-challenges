cbf9bc276462841d40a3a279d63edbd07a81fa97
user1808 <tomasz.kozak04@gmail.com>
2024-03-25 18:21:20 +0100

    462 - currying 2

--- questions/00462-extreme-currying-2/template.ts
+++ questions/00462-extreme-currying-2/template.ts
@@ -1,1 +1,11 @@
-declare function DynamicParamsCurrying(fn: any): any
+type Curry2<Args, Result, D extends unknown[] = []> =
+    Args extends [infer H, ...infer T]
+      ? T extends []
+        ? (...args: [...D, H]) => Result
+        : ((...args: [...D, H]) => Curry2<T, Result>) & Curry2<T, Result, [...D, H]>
+      : () => Result
+
+declare function DynamicParamsCurrying<
+  Args extends unknown[], 
+  Result,
+>(fn: (...args: Args) => Result): Curry2<Args, Result>;
