c35e158b421ee3f2557b50c9fc520d5162c49d5d
user1808 <tomasz.kozak04@gmail.com>
2024-03-26 20:55:55 +0100

    32532 - binary addition

--- questions/32532-hard-binary-addition/template.ts
+++ questions/32532-hard-binary-addition/template.ts
@@ -1,3 +1,22 @@
-type Bit = 1 | 0
+type Bit = 1 | 0;
 
-type BinaryAdd<A extends Bit[], B extends Bit[]> = any
+type BitNegation<T extends Bit> = T extends 1 ? 0 : 1;
+type AddBits<A extends Bit, B extends Bit, Rest extends Bit = 0> = 
+  A extends 0
+    ? B extends 0 ? [0, Rest] : [Rest, BitNegation<Rest>]
+    : B extends 0 ? [Rest, BitNegation<Rest>] : [1, Rest];
+
+type BinaryAdd<A extends Bit[], B extends Bit[], Rest extends Bit = 0, Acc extends Bit[] = []> = 
+  A extends [...infer TailA extends Bit[], infer HeadA extends Bit]
+    ? B extends [...infer TailB extends Bit[], infer HeadB extends Bit]
+      ? AddBits<HeadA, HeadB, Rest> extends infer Result extends [Bit, Bit]
+        ? BinaryAdd<TailA, TailB, Result[0], [Result[1], ...Acc]>
+        : never
+      : AddBits<HeadA, 0, Rest> extends infer Result extends [Bit, Bit]
+        ? BinaryAdd<TailA, [], Result[0], [Result[1], ...Acc]>
+        : never
+    : B extends [...infer TailB extends Bit[], infer HeadB extends Bit]
+      ? AddBits<0, HeadB, Rest> extends infer Result extends [Bit, Bit]
+        ? BinaryAdd<[], TailB, Result[0], [Result[1], ...Acc]>
+        : never
+      : Rest extends 0 ? Acc : [Rest, ...Acc];
